74ca442def71aae33db265e44b6dedd8209d290f
Justin Liu <justinwl2097@gmail.com>
2022-09-11 11:10:37 -0500

    finished manager tests

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,5 +1,34 @@
 const Manager = require('../lib/Manager');
 
+describe('Manager Inherited Properties and Methods', () => {
+    describe('getName', () => {
+        it('should return the defined name', () => {
+            const name = "Alice";
+
+            const mgmt = new Manager("Alice", 0, "", 101);
+
+            expect(mgmt.getName()).toEqual(name);
+        })
+    })
+    describe('getId', () => {
+        it('should return the defined Id', () => {
+            const id = 1234;
+
+            const mgmt = new Manager("", 1234, "", 101);
+
+            expect(mgmt.getId()).toEqual(id);
+        })
+    })
+    describe('getEmail', () => {
+        it('should return the defined Email', () => {
+            const email = "test@email.com";
+
+            const mgmt = new Manager("", 0, "test@email.com", 101);
+
+            expect(mgmt.getEmail()).toEqual(email);
+        })
+    })
+})
 describe('Manager Properties and Getters', () => {
     describe('getOfficeNumber', () => {
         it('should return the defined office number', () => {
@@ -16,5 +45,12 @@ describe('Manager Properties and Getters', () => {
 
             expect(mgmt.getRole()).toEqual("Manager");
         })
+    })
+})
+describe('Passing invalid parameters', () => {
+    it('should return an empty object when passing an invalid office number', () => {
+        const mgmt = new Manager("Bob", 1234, "bob@email.com", "888");
+
+        expect(mgmt).toEqual({});
     })
 })
